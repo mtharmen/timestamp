c2f07188b0b3df5ddcd98a65340fbff168123ad3
Muhinthan <m.tharmendran@gmail.com>
2016-09-13 20:54:55 +0000

    Last commit

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 var express = require('express');
-
-var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function formatDate(date) {
+  var year = date.getFullYear();
+  var month = ['January','February','March','April','May','June','July','August','September','October','November','December'][date.getMonth()];
+  var day = date.getDate();
+  return month + ' ' + day + ', ' + year;
+}
+
+function ParseDate(raw) {
+  
+  var parsed = {unix: null, natural: null};
+  
+  // Checking to see if it's purely numbers, ie in unix time
+  if (raw.match(/^[0-9]+$/) !== null) {
+    raw *= 1000; //convert to milliseconds
+  }
+  var date = new Date(raw);
+  
+  if (date.toString() === 'Invalid Date') {
+    return parsed;
+  }
+  
+  parsed.unix = date.getTime() / 1000;
+  parsed.natural = formatDate(date);
+  return parsed;
+}
+
+app.use('/', express.static(__dirname + '/styles'));
+
+app.get('/:date', function(req, res){
+  var rawDate = req.params.date;
+  var parsed = ParseDate(rawDate);
+
+  res.json(parsed);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(8080);
