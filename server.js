64560e96526f37dd62eea8b48d4e66c9f96ba7c5
mtharmen <m.tharmendran@gmail.com>
2018-11-16 15:21:32 -0500

    Update for new curriculum

--- server.js
+++ server.js
@@ -1,42 +1,28 @@
-var express = require('express');
-var app = express();
+const path = require('path')
+const express = require('express')
+const app = express()
 
-function formatDate(date) {
-  var year = date.getFullYear();
-  var month = ['January','February','March','April','May','June','July','August','September','October','November','December'][date.getMonth()];
-  var day = date.getDate();
-  return month + ' ' + day + ', ' + year;
-}
-
-function ParseDate(raw) {
-  
-  var parsed = {unix: null, natural: null};
-  
-  // Checking to see if it's purely numbers, ie in unix time
-  if (raw.match(/^[0-9]+$/) !== null) {
-    raw *= 1000; //convert to milliseconds
+function parseString (raw) {
+  var date = new Date()
+  if (raw.indexOf('-') > -1) {
+    date = new Date(raw)
+  } else if (date) {
+    date = new Date(parseInt(raw))
   }
-  var date = new Date(raw);
-  
+
   if (date.toString() === 'Invalid Date') {
-    return parsed;
+    return { 'error': date.toString() }
   }
-  
-  parsed.unix = date.getTime() / 1000;
-  parsed.natural = formatDate(date);
-  return parsed;
+
+  return { unix: date.getTime(), utc: date.toUTCString() }
 }
 
-app.use('/', express.static(__dirname + '/styles'));
+app.use('/', express.static(path.join(__dirname, '/views')))
 
-app.get('/:date', function(req, res){
-  var rawDate = req.params.date;
-  var parsed = ParseDate(rawDate);
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  var rawDate = req.params.date_string
+  res.json(parseString(rawDate))
+})
 
-  res.json(parsed);
-});
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-    console.log('Listening on port ', port);
-});
+var port = process.env.PORT || 8080
+app.listen(port, () => { console.log('Listening on port ', port) })
